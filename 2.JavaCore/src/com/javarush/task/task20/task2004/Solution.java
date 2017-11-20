56257d3b08619121089632975b8a44e9d086de2a
si6erian <nassi.aka@gmail.com>
2017-11-20 17:46:06 +0300

    2004 done

---
+++ 2.JavaCore/src/com/javarush/task/task20/task2004/Solution.java
@@ -0,0 +1,87 @@
+package com.javarush.task.task20.task2004;
+
+import java.io.*;
+
+/* 
+Читаем и пишем в файл статики
+*/
+public class Solution {
+    public static void main(String[] args) {
+        //you can find your_file_name.tmp in your TMP directory or fix outputStream/inputStream according to your real file location
+        //вы можете найти your_file_name.tmp в папке TMP или исправьте outputStream/inputStream в соответствии с путем к вашему реальному файлу
+        try {
+            File your_file_name = File.createTempFile("2004-",".txt", new File("/home/dl/Dropbox/DINS/java/"));
+            OutputStream outputStream = new FileOutputStream(your_file_name);
+            InputStream inputStream = new FileInputStream(your_file_name);
+
+            ClassWithStatic classWithStatic = new ClassWithStatic();
+            classWithStatic.i = 3;
+            classWithStatic.j = 4;
+            classWithStatic.save(outputStream);
+            outputStream.flush();
+
+            ClassWithStatic loadedObject = new ClassWithStatic();
+            loadedObject.staticString = "something";
+            loadedObject.i = 6;
+            loadedObject.j = 7;
+
+            loadedObject.load(inputStream);
+            //check here that classWithStatic object equals to loadedObject object - проверьте тут, что classWithStatic и loadedObject равны
+            System.out.println(loadedObject.equals(classWithStatic));
+
+            outputStream.close();
+            inputStream.close();
+
+        } catch (IOException e) {
+            //e.printStackTrace();
+            System.out.println("Oops, something wrong with my file");
+        } catch (Exception e) {
+            //e.printStackTrace();
+            System.out.println("Oops, something wrong with save/load method");
+        }
+    }
+
+    public static class ClassWithStatic {
+        public static String staticString = "it's test static string";
+        public int i;
+        public int j;
+
+        public void save(OutputStream outputStream) throws Exception {
+            //implement this method - реализуйте этот метод
+            PrintWriter writer = new PrintWriter(outputStream);
+            writer.println(staticString);
+            writer.println(String.valueOf(i));
+            writer.println(String.valueOf(j));
+            writer.flush();
+            writer.close();
+        }
+
+        public void load(InputStream inputStream) throws Exception {
+            //implement this method - реализуйте этот метод
+            BufferedReader reader = new BufferedReader(new InputStreamReader(inputStream));
+            staticString = reader.readLine();
+            this.i = Integer.parseInt(reader.readLine());
+            this.j = Integer.parseInt(reader.readLine());
+            reader.close();
+        }
+
+        @Override
+        public boolean equals(Object o) {
+            if (this == o) return true;
+            if (o == null || getClass() != o.getClass()) return false;
+
+            ClassWithStatic that = (ClassWithStatic) o;
+
+            if (i != that.i) return false;
+            return j == that.j;
+
+        }
+
+        @Override
+        public int hashCode() {
+            int result = i;
+            result = 31 * result + j;
+            return result;
+        }
+    }
+}

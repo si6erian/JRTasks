6f2376978e3d12ff86a12f4736d55bc77bb30e40
si6erian <nassi.aka@gmail.com>
2017-12-01 18:13:04 +0300

    2017 is done

---
+++ 2.JavaCore/src/com/javarush/task/task20/task2017/Solution.java
@@ -0,0 +1,40 @@
+package com.javarush.task.task20.task2017;
+
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.Serializable;
+
+/* 
+Десериализация
+*/
+public class Solution {
+    public A getOriginalObject(ObjectInputStream objectStream) {
+
+        try {
+            A a = (A) objectStream.readObject();
+            return a;
+        } catch (IOException e) {
+            e.printStackTrace();
+            return null;
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+            return null;
+        } catch (Exception e) {
+            e.printStackTrace();
+            return null;
+        }
+    }
+
+    public class A implements Serializable{
+    }
+
+    public class B extends A {
+        public B() {
+            System.out.println("inside B");
+        }
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
